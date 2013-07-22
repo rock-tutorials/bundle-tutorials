d7efebd95668398e770f2572655aee50a3c5324e
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-07-22 10:47:00 +0200

    fix the definitions after the introduction of leader/follower
    Everything but leader and follower definitions were broken because of
    ambiguous deployment problems

--- models/profiles/rocks.rb
+++ models/profiles/rocks.rb
@@ -5,15 +5,23 @@ using_task_library 'tut_follower'
 
 module Tutorials
   profile 'Rocks' do
-    define 'joystick',    Tutorials::RockControl.use(Controldev::JoystickTask)
-    define 'random',      Tutorials::RockControl.use(TutBrownian::Task)
-    define 'random_slow', Tutorials::RockControl.use(TutBrownian::Task.with_conf('default', 'slow'))
-    define 'random_slow2', Tutorials::RockControl.use(TutBrownian::Task).with_conf('slow')
-    define 'leader',
-      Tutorials::RockControl.use(TutBrownian::Task).
-        use_deployments(/target/)
-    define 'follower',
-      Tutorials::RockControl.use(TutFollower::Task, leader_def).
-        use_deployments(/follower/)
+      define 'joystick',    Tutorials::RockControl.
+          use(Controldev::JoystickTask).
+          use_deployments(/target/)
+      define 'random',      Tutorials::RockControl.
+          use(TutBrownian::Task).
+          use_deployments(/target/)
+      define 'random_slow', Tutorials::RockControl.
+          use(TutBrownian::Task.with_conf('default', 'slow')).
+          use_deployments(/target/)
+      define 'random_slow2', Tutorials::RockControl.
+          use(TutBrownian::Task).with_conf('slow').
+          use_deployments(/target/)
+      define 'leader', Tutorials::RockControl.
+          use(TutBrownian::Task).
+          use_deployments(/target/)
+      define 'follower', Tutorials::RockControl.
+          use(TutFollower::Task, leader_def).
+          use_deployments(/follower/)
   end
 end
