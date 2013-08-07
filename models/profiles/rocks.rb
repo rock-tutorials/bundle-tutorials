6a39d604eaf2cbb7a91aed3e53ebfd41de80af0b
Sylvain Joyeux <sylvain.joyeux@dfki.de>
2013-08-07 19:51:27 +0200

    simplify the result greatly by adding the use_deployments on the devices

--- models/profiles/rocks.rb
+++ models/profiles/rocks.rb
@@ -7,30 +7,25 @@ module Tutorials
     profile 'Rocks' do
         robot do
             device Dev::Controldev::Joystick, :as => 'joystick'
-            device Dev::Platforms::Rock, :as => 'rock1'
-            device Dev::Platforms::Rock, :as => 'rock2'
+            device(Dev::Platforms::Rock, :as => 'rock1').
+                use_deployments(/target/)
+            device(Dev::Platforms::Rock, :as => 'rock2').
+                use_deployments(/follower/)
         end
 
         define 'joystick',    Tutorials::RockControl.
-            use(joystick_dev, rock1_dev).
-            use_deployments(/target/)
+            use(joystick_dev, rock1_dev)
         define 'random',      Tutorials::RockControl.
-            use(TutBrownian::Task, rock1_dev).
-            use_deployments(/target/)
+            use(TutBrownian::Task, rock1_dev)
         define 'random2',      Tutorials::RockControl.
-            use(TutBrownian::Task, rock2_dev).
-            use_deployments(/follower/)
+            use(TutBrownian::Task, rock2_dev)
         define 'random_slow', Tutorials::RockControl.
-            use(TutBrownian::Task.with_conf('default', 'slow'), rock1_dev).
-            use_deployments(/target/)
+            use(TutBrownian::Task.with_conf('default', 'slow'), rock1_dev)
         define 'random_slow2', Tutorials::RockControl.
-            use(TutBrownian::Task, rock1_dev).with_conf('slow').
-            use_deployments(/target/)
+            use(TutBrownian::Task, rock1_dev).with_conf('slow')
         define 'leader', Tutorials::RockControl.
-            use(TutBrownian::Task, rock1_dev).
-            use_deployments(/target/)
+            use(TutBrownian::Task, rock1_dev)
         define 'follower', Tutorials::RockControl.
-            use(TutFollower::Task, rock2_dev, 'target_pose' => leader_def).
-            use_deployments(/follower/)
+            use(TutFollower::Task, rock2_dev, 'target_pose' => leader_def)
     end
 end
